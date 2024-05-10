6603439fab31b43ef12a19d5c168eda4d3d4db70
Мамаду <fadigamamadou55@gmail.com>
2024-05-10 19:35:37 +0300

    дабавление задание в решение

--- zadanie_10_tect_naabarot.py
+++
@@ -1,3 +0,0 @@
-tect = input("Введите тест:")
-tect_reverse = tect[::-1]
-print(tect_reverse)
--- zadanie_9_procbi_polzavatel.py
+++
@@ -1,5 +0,0 @@
-start = int(input("Введите начало:"))
-finish = int (input("Введите канеч:"))
-interval = int(input("Введите интервал:"))
-for i in range(start,finish,interval):
-    print(i)
