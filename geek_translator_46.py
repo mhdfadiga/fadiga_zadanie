ee07b0f96d11537ff651cf825a9dc3fa9fdd9d78
Мамаду <fadigamamadou55@gmail.com>
2024-05-18 14:24:43 +0300

    окончание задача

--- geek_translator_46.py
+++ geek_translator_46.py
@@ -1,3 +1,10 @@
+geek = {"404": "Не знать. не владеть информацией. От сообщения об ошибке 404 'Страница не найдена'.",
+        "Googling": "'Гугление·. nоиск в Сети сведений о ком-либо.",
+        "Keyboard Pl aque" : "Мусор. который скапливается в клавиатуре компьютера.",
+        "Link Rot" : "Процесс устаревания гиперссылок на веб-страницах . ",
+        "Percussive Maintenance" : "О ситуации. когда кто-либо бьет по корпусу неисправного электронного прибора в надежде восстановить его работу.",
+        "Uninstalled" : "Об увольнении кого-либо. Особенно популярно на рубеже 1990-2000-х годов "
+         }
 choice = None
 while choice != "0" :
     print("""
@@ -17,6 +24,29 @@ while choice != "0" :
         term = input("Kaкoй термин вы хотите перевести с гикского на русский? ")
         if term in geek:
             definition = geek[term]
-            print("\n".term,"означает",definition)
+            print("\n",term,"означает",definition)
         else:
-            print("\nYвы. этот термин мне незнаком:", term)
+            print("\nYвы,этот термин мне незнаком:", term)
+    elif choice == "2":
+        term = input("Kaкoй термин гикского языка вы хотите добавить? ")
+        if term not in geek:
+            definition = input("nВпишите ваше толкование:")
+            geek[term] = definition
+            print("\nTepмин",term,"добавлен в словарь.")
+        else:
+            print("\nTaкoй термин уже есть! Попробуйте изменить его толкование.")
+    elif choice == "3":
+        term = input("Kaкoй термин вы хотите переопределить? ")
+        if term in geek:
+            definition = input("Впишите ваше толкование: ")
+            geek[term] = definition
+            print("\nTepмин",term,"переопределен.")
+        else:
+            print("\nTaкoгo термина пока нет! Попробуйте добавить его в словарь.")
+    elif choice == "4":
+        term = input("Kaкoй термин вы хотите удалить? ")
+        if term in geek:
+            del geek[term]
+            print("\nTepмин", term,"удален.")
+    else:
+        print("\nHичeм не могу помочь. Термина",term, "нет в словаре.")
